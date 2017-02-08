912b60bad155e55d24fe76a8aa3179c3f19e3e0f
stanley <szheng1689@gmail.com>
2017-02-08 12:23:08 -0500

    Changed 'tabs' to 'space'

--- static/js/main.js
+++ static/js/main.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
 
 
-	$("#home-page").show();
-	$("#portfolio-page").hide();
-	$("#about-page").hide();
+    $("#home-page").show();
+    $("#portfolio-page").hide();
+    $("#about-page").hide();
 
-	//Display different pages for each tab
-	$("#nav-home").click(function(){
-		$("#home-page").show();
-		$("#portfolio-page").hide();
-		$("#about-page").hide();
-	});
-	$("#nav-portfolio").click(function(){
-		$("#home-page").hide();
-		$("#portfolio-page").show();
-		$("#about-page").hide();
-	});
-	$("#nav-about").click(function(){
-		$("#home-page").hide();
-		$("#portfolio-page").hide();
-		$("#about-page").show();
-	});
+    //Display different pages for each tab
+    $("#nav-home").click(function(){
+        $("#home-page").show();
+        $("#portfolio-page").hide();
+        $("#about-page").hide();
+    });
+    $("#nav-portfolio").click(function(){
+        $("#home-page").hide();
+        $("#portfolio-page").show();
+        $("#about-page").hide();
+    });
+    $("#nav-about").click(function(){
+        $("#home-page").hide();
+        $("#portfolio-page").hide();
+        $("#about-page").show();
+    });
 
 });
